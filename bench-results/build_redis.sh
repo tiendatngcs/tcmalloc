66bcf2d90bcc47cc8f7d84756dd00a7edf40b488
tiendatngcs <tiendat.ng.cs@gmail.com>
2021-08-03 13:09:39 -0500

    Mod build redis in bench-result

--- bench-results/build_redis.sh
+++ bench-results/build_redis.sh
@@ -1,10 +1,8 @@
-REDIS_SRC=$1
-TCMALLOC_BIN=$2
-
 echo "Make sure you run below"
-echo make USE_JEMALLOC=no MALLOC_LIB=-L$TCMALLOC_BIN -ltcmalloc
+
+echo make USE_JEMALLOC=no MALLOC_LIB='-L'$TCMALLOC_BIN' -ltcmalloc'
 
 cd $REDIS_SRC
-make USE_JEMALLOC=no MALLOC_LIB=-L$TCMALLOC_BIN -ltcmalloc
+make USE_JEMALLOC=no MALLOC_LIB="-L${TCMALLOC_BIN} -ltcmalloc"
 
 echo export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$TCMALLOC_BIN
