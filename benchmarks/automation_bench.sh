5c645de2f157cfd3149502d993cda999ca71f93a
Minh Dang <minhdangtn@tamu.edu>
2021-08-25 12:48:47 -0500

    update automation bench

--- benchmarks/automation_bench.sh
+++ benchmarks/automation_bench.sh
@@ -1,5 +1,5 @@
 # this script will run the bench_profile in redis-custom-bench and minh-custom-bench
-BENCHMARK=("redis" "Producer-Consumer")
+BENCHMARK=("Producer-Consumer")
 RELEASE_RATE=("0MB" "1MB" "10MB")
 TEST_NAME=("SET")
 PROFILE_NAME=("Beta" "Bravo" "Charlie" "Delta" "Echo" "Foxtrot" "Merced" "Sierra" "Sigma" "Uniform")
@@ -33,10 +33,10 @@ do
         then
             for test_name in "${TEST_NAME[@]}"
             do
-                temp="$REDIS_BENCH_DIR/./bench_profile $bench $release_rate $test_name"
+                temp="$bench $release_rate $test_name"
             done
         else
-            temp="$PROD_CONS_BENCH_DIR/./hello_world $bench $release_rate"
+            temp="$bench $release_rate"
         fi
 
         for profile in "${PROFILE_NAME[@]}"
@@ -44,7 +44,17 @@ do
             for drain_cycle in "${DRAIN_CYCLE[@]}"
             do
                 run="$temp $profile $drain_cycle"
-                echo $run
+                echo "Running: $bench $release_rate $test_name $profile $drain_cycle"
+                if [ $bench = "redis" ]
+                then
+                    cd $REDIS_BENCH_DIR/
+                    ./bench_profile $run
+                else
+                    cd $PROD_CONS_BENCH_DIR/
+                    ./hello_world $run
+                fi
+                echo "Done"
+                echo "-----------------------------------------------------------------------------------------"
             done
         done
     done
